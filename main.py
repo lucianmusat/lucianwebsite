79872bba5ca68cfd992f6bc1b27a0becfaa96b79
Lucian Musat <amibadesign@gmail.com>
2023-11-10 14:47:43 +0100

    Add contact page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, status, Query
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -19,3 +19,18 @@ async def root(request: Request):
 @app.get("/resume")
 async def root(request: Request):
     return templates.TemplateResponse("resume.html", {"request": request})
+
+
+@app.get("/contact")
+async def root(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request,
+               "message": """No boring old contact form here!<br /><br />You can contact me at 
+               <a href="mailto:lucian.musat@hotmail.com">lucian.musat@hotmail.com</a> or check out my 
+               <a href="https://www.linkedin.com/in/lucian-musat/" target="_blank">LinkedIn</a>
+                or <a href="https://github.com/lucianmusat" target="_blank">GitHub</a> profiles."""})
+
+
+@app.exception_handler(404)
+async def custom_404_handler(request, __):
+    return templates.TemplateResponse("contact.html", {"request": request,
+                                                       "message": "Not sure what you are looking for, but it's not here!"})
